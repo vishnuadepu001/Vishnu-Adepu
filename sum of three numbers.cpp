26c413ce12d15c87f71e5cccb1c4c43bd4976a02
Vishnu Prasad Adepu <110533980+vishnuadepu001@users.noreply.github.com>
2022-08-25 22:20:10 +0530

    Add files via upload

---
+++ sum of three numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a ,b , c, d;
+    cout<<"ENTER THREE NUMBERS =";
+    cin>>a>>b>>c;
+    d=a+b+c;
+    cout<<"sum is ="<<d;
+    
+}
