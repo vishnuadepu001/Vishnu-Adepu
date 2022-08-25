ed6d995982d30fc3d05286b79ede8b126f5f245e
Vishnu Prasad Adepu <110533980+vishnuadepu001@users.noreply.github.com>
2022-08-25 22:24:48 +0530

    Add files via upload

---
+++ scope resolution outside the class.cpp
@@ -0,0 +1,29 @@
+//outside the class
+#include<iostream>
+using namespace std;
+class student
+{
+	int rollno;
+	char name [30];
+	public:
+		void getdata();
+		void showdata();
+		
+};
+//scope resolution operator ::
+void student::getdata()
+{
+	cout<<"enter the values of rollno and name";
+	cin>>rollno>>name;
+}
+void student::showdata()
+{
+	cout<<"\n rollno is="<<rollno;
+	cout<<"\n name is="<<name;
+	
+}
+main()
+{
+	class student obj;
+	obj.getdata();	obj.showdata();
+}
